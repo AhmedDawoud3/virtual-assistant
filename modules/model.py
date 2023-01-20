658c826effd3aff64b94835c331be8c09bc7780d
Mostafa Ehab <Mostafa-Ehab>
2023-01-20 22:48:39 +0200

    Fixing some errors - initiate app.py with print and input functions - Change return statement in model.py to be able to control app.py - temporarily hang instant shutdown and restart till add a suitable function for such multiple repond modules

--- modules/model.py
+++ modules/model.py
@@ -7,6 +7,7 @@ from modules.prayer.prayer import Prayer
 from modules.search.open import Open
 from modules.search.wiki import Wiki
 from modules.search.close import Close
+from modules.download.download import Download
 from modules.system.brightness import Brightness
 from modules.system.shutdown import Shutdown
 from modules.system.restart import Restart
@@ -23,23 +24,23 @@ class Model:
 
         calc = Calculate()
         if calc.can_calculate(text):
-            return calc.get_result()
+            return (True, calc.get_result())
 
         elif "weather" in text:
-            return Weather(self.city)
+            return (True, Weather(self.city))
 
         elif "date" in text and "update" not in text:
-            return f"Today is {strftime('%A')} {int(strftime('%d'))} {strftime('%B')} {strftime('%Y')}"
+            return (True, f"Today is {strftime('%A')} {int(strftime('%d'))} {strftime('%B')} {strftime('%Y')}")
 
         elif "time" in text:
-            return f"It's {int(strftime('%I'))} {int(strftime('%M'))} {strftime('%p')}"
+            return (True, f"It's {int(strftime('%I'))} {int(strftime('%M'))} {strftime('%p')}")
 
         elif "pray" in text or "azan" in text:
-            return Prayer(self.city)
+            return (True, Prayer(self.city))
 
         elif "open" in text:
             text = text.replace("open ", "")
-            return Open.search(text)
+            return (True, Open.search(text))
 
         elif "what is" in text or "what's" in text or "who is" in text or "who's" in text:
             text = text.replace("what is ", "")
@@ -48,35 +49,54 @@ class Model:
             text = text.replace("who's ", "")
 
             wiki = Wiki()
-            return wiki.wiki_search(text)
+            if wiki.wiki_search(text):
+                return (True, wiki.get_result())
+            else:
+                return (True, "This is some results from the web")
 
         elif "google" in text:
             text = text.replace("google ", "")
-            return Wiki.google_search(text)
+            Wiki.google_search(text)
+            return (True, "This is some results from the web")
+
+        # elif "download" in text:
+        #     text = text.replace("download ", "")
+        #     download = Download()
+        #     if "youtube" in text:
+        #         text = text.replace("youtube", "")
+        #         if download.youtube_search():
+        #             download.youtube_download()
+        #     elif "music" in text:
+        #         text = text.replace("music", "")
 
         elif "brightness" in text:
             brightness = Brightness()
             if "increase" in text or "raise" in text:
-                return brightness.increase()
+                return (True, brightness.increase())
 
             elif "decrease" in text or "lower" in text:
-                return brightness.decrease()
+                return (True, brightness.decrease())
 
             elif "max" in text:
-                return brightness.maximum()
+                return (True, brightness.maximum())
 
             elif "min" in text:
-                return brightness.minimum()
+                return (True, brightness.minimum())
 
             else:
-                return brightness.get_current_value()
+                return (True, f"Current brightness is {brightness.get_current_value()}")
 
         elif "close" in text:
             text = text.replace("close ", "")
-            return Close.search(text)
+            if Close.search(text):
+                return (True, f"Closing {text}")
+            # return (True, Close.search(text))
 
         elif "shutdown" in text or "shut down" in text:
-            return Shutdown(text)
+            shutdown = Shutdown()
+            return shutdown.shutdown(text)
 
         elif "restart" in text:
-            return Restart(text)
+            restart = Restart()
+            return restart.restart(text)
+        return (False, "Sorry I didn't understand")
