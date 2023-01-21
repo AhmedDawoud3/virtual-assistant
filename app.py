87633f3abd6c5aaa2548fe6ff0461c61d00a3a95
Mostafa Ehab <Mostafa-Ehab>
2023-01-21 02:01:22 +0200

    Download youtube and download music has returned to work correctly

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
 from modules.model import Model
 
-model = Model()
 
-while True:
-    command = input("Enter your command: ")
-    respond = model.process(command)
-    if respond[0]:
-        print(respond[1])
+class Main():
+    def main(self):
+        model = Model()
+
+        while True:
+            command = self._in("Enter your command: ")
+            respond = model.process(command)
+            if respond[0]:
+                self._out(respond[1])
+            else:
+                respond[1](self._in, self._out)
+
+    def _in(self, text):
+        return input(text)
+
+    def _out(self, text):
+        print(text)
+
+
+if __name__ == "__main__":
+    main = Main()
+    main.main()
