8f99917c924b9de581d24b144b98d8e99aa17743
Mostafa Ehab <Mostafa-Ehab>
2023-01-23 04:08:51 +0200

    merge conflicts

--- app.py
+++ app.py
@@ -25,9 +25,8 @@ class Main():
         gui.daemon = True
         gui.start()
 
-        # make sure that GUI has been initialized
-        while not self.gui.is_ready:
-            continue
+        # Start speech recognition module
+        self.recognise = sr.Recognizer()
 
         # Check user data
         model.check_user_data(self. _in, self._out, self.engine, self.v)
