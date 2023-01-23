3dc97e7e7ad506d1608d3b6d665846025b8cf129
Ahmed Dawoud <68483546+AhmedDawoud3@users.noreply.github.com>
2023-01-23 03:33:55 +0200

    Fix user data

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ class Main():
         gui.start()
 
         # Check user data
-        model.check_user_data(self. _in, self._out, self.engine, self.v)
+        threading.Thread(model.check_user_data(self. _in, self._out, self.engine, self.v))
 
         # make sure that GUI has been initialized
         while not self.gui.is_ready:
